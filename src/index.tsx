fe202b84cb112a5eda748665843ae731d2a378fa
Bruemor <arymer1@gmail.com>
2020-09-24 18:26:56 -0400

    Brought in BrowserRouter to fix error from Navigation component

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,12 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 import App from './app/App';
 
-ReactDOM.render (
-    <App color="blue" />,
-    document.getElementById('root')
+ReactDOM.render(
+  <BrowserRouter>
+    <App color="blue" />
+  </BrowserRouter>,
+  document.getElementById('root')
 );
